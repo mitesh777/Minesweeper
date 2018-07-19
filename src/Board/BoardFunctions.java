47da44cbf9cf32430dacfc6a237f0248a8e67971
Admin <miteshkakkar8@gmail.com>
2018-07-19 09:35:42 +0530

    initial commit

---
+++ src/Board/BoardFunctions.java
@@ -0,0 +1,74 @@
+package Board;
+
+import java.util.Random;
+
+public class BoardFunctions {
+	
+	public static int[][] fillArray(int rows, int minesNumber) {
+		int[][] mines=new int[rows][rows];
+		
+		for(int i=0;i<minesNumber;i++) {
+			int randomNumber=generateRandomFunction(rows*rows-1);
+			System.out.print(randomNumber+" ");
+			if(mines[randomNumber/rows][randomNumber%rows]==-1) {
+				--i;
+				System.out.println("same");
+				continue;
+			}
+			System.out.println("ok");
+			mines[randomNumber/rows][randomNumber%rows]=-1;
+		}
+		return mines;
+	}
+	
+	public static int generateRandomFunction(int max) {
+		// TODO Auto-generated method stub
+		// create instance of Random class
+		Random randomNum = new Random();
+		int num = randomNum.nextInt(max);
+		return num;
+	}
+	
+	public static int[][] fillWithNumbers(int[][] arr, int n) {
+		// TODO Auto-generated method stub
+		//int[][] output = new int[n][n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(arr[i][j]!=-1) {
+					countNeighbouringMines(arr,i,j);
+				}
+			}
+		}
+		return arr;
+	}
+	
+	private static void countNeighbouringMines(int[][] arr,int row,int col) {
+		// TODO Auto-generated method stub
+		//int ans=0;
+		for(int i=-1;i<2;i++) {
+			for(int j=-1;j<2;j++) {
+				if((row+i>=0 && row+i<=7) && (col+i>=0 && col+i<=7)) {
+					if(arr[row+i][col+i]==-1) {
+						++arr[row][col];
+					}
+				}
+			}
+		}
+		
+		//return ans;
+	}
+
+	public static void main(String[] args) {
+		int[][] arr = fillArray(8,10);
+		int[][] out = fillWithNumbers(arr,8);
+		for(int i=0;i<8;i++) {
+			for(int j=0;j<8;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	
+}
